dd63e2dc69aafd18cc94c4965d748a3b57506e37
Bob Calco <rcalco@mcna.net>
2025-08-14 15:20:56 -0400

    added nvim-cmp, duh

--- lua/custom/plugins/conjure.lua
+++ lua/custom/plugins/conjure.lua
@@ -14,6 +14,37 @@ return {
     dependencies = { 'PaterJason/cmp-conjure' },
   },
   {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp', -- LSP completion source
+      'hrsh7th/cmp-buffer', -- Buffer completion source
+      'hrsh7th/cmp-path', -- Path completion source
+      'hrsh7th/cmp-cmdline', -- Command-line completion source
+      -- Add other cmp sources as needed, e.g., for snippets
+      -- "L3MON4D3/LuaSnip",
+      -- "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      -- Your nvim-cmp setup goes here
+      local cmp = require 'cmp'
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            -- Configure your snippet engine here, e.g., LuaSnip
+            -- require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        sources = cmp.config.sources {
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+          { name = 'path' },
+          { name = 'cmdline' },
+        },
+        -- Add other nvim-cmp configurations like mappings, formatting, etc.
+      }
+    end,
+  },
+  {
     'PaterJason/cmp-conjure',
     lazy = true,
     config = function()
